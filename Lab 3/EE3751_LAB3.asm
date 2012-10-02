f22d8b28cc62497eff213493e055ef8309025e3a
popasquat89 <popasquat89@gmail.com>
2012-10-01 23:36:19 -0500

    Everything working except Conversion the commit message for your changes. Lines starting

--- Lab 3/EE3751_LAB3.asm
+++ Lab 3/EE3751_LAB3.asm
@@ -1,7 +1,9 @@
+
+
 data segment      
     
-AGREETING db 0Dh, 'Enter A:$'
-BGREETING db 0Ah, 'Enter B:$'
+AGREETING db 'Enter A:$'
+BGREETING db 0Ah , 'Enter B:$'
 A           db  4  DUP(?)
 B           db  4  DUP(?) 
 SUM         db  4  DUP(?)         
@@ -18,12 +20,17 @@ code segment
     
     main proc far 
         
-        mov AX, data
-        mov DS, ax
-        mov ES, ax    
-        
+        mov ax, data
+        mov ds, ax
+        mov es, ax
+    
         call Retrieve_A 
-        call Retrieve_B
+         
+        call Retrieve_B 
+        
+        call ADD_A_B
+        
+        call AminuB
         
         
         call EXIT
@@ -35,37 +42,30 @@ code segment
     
     ;Special call for retrieving A, converting it then returning it;
     Retrieve_A proc near:
-        
-                      
-        mov AH, 9h              
-        mov DX, offset AGREETING ;set DX to the address of A Messae
-        int 21h       
-        
+               
+        mov DX, offset AGREETING ;set DX to the address of A
+        mov AH, 9h
+           
+        int 21h
         call INPUT_NUMBER 
-        call CONV_ASC2BIN 
-         
-        lea SI, A
-        call TRANSFER_TO_NUM
+        lea DI, A
+        call CONV_ASC2BIN
         call Erase_Variables
         ret
         
     Retrieve_A endp 
     
     
-    ;Special call for retrieving B, converting it then returning it;
+    ;Special call for retrieving A, converting it then returning it;
     Retrieve_B proc near:
+
         
-        
-        mov AH, 09h
-        mov DX, offset BGREETING ; set DX to the address of B Message
-            
-        
+        mov DX, offset BGREETING ; set DX to the address of B    
+        mov AH, 9h
         int 21h
         call INPUT_NUMBER
-        call CONV_ASC2BIN 
-        
-        lea SI, B 
-        call TRANSFER_TO_NUM
+        lea DI, B
+        call CONV_ASC2BIN  
         ;call Erase_Variables
         ret    
         
@@ -76,12 +76,12 @@ code segment
     ;Grab the number from the user;
     INPUT_NUMBER proc near
         mov CX, 00h
-        lea DI, NUMBER
+        mov DI, offset NUMBER
         
         
         mov BX, 0
         getNumber:
-        mov AH, 01h ;Grabs the number storing count in BX, SI pointing to number
+        mov AH, 01h ;Grabs the number storing count in BX, DI pointing to number
         int 21h      
         
         cmp AL, 0Dh    ; see if the user hits Enter or 'q'/ 'Q'
@@ -104,68 +104,158 @@ code segment
     ; Convert from ASCII to Binary ;             
     CONV_ASC2BIN proc near
         
-        sub DI, BX
-        
-        mov CX, BX
-        subtraction:
-            sub [DI], 30h
-            inc DI  
-            
-        loop subtraction
-        
-        sub DI, BX
-        
-        mov CX, BX 
-        mov AX, 1h
+    mov SI,offset NUMBER
+    
+    mov CX,9
+    ;MOVING ALL NUMBER TO DECIMAL
+    Here:
+        sub [SI],30H
+        inc SI
+    loop Here ;END CONVERSION TO BINARY
+    
+     
+    
+    ; CREATE TABLE FROM 10^0 TO 10^9
+    mov CX,8
+    
+    mov BX,10d
+    mov AX,01 
+    mov DX,0 
+    push DX
+    push AX
+    
+    Table:
+        mul BX
+        push DX
         push AX
-        timesTen:
-            mul 10
+    loop table 
+    
+    ;FIXING THE MSB FROM TABLE
+    mov CX,8 
+    add SP,34d
+    
+    FIX_MSB:
+    
+        pop AX
+        mul BL
+        sub SP,6
+        pop DX
+        add AX,DX
+        push AX
+    
+    
+    loop FIX_MSB ;END CREATE TABLE/FIXING
+    sub SP,2
+    
+    ;BEGINING MULTIPLICATION AND MOVING DESTINATION
+    mov SI,offset NUMBER
+    clc
+    pushf
+    
+    Here2:
+    
+        mov AX,[SI]
+        and AX,0Fh
+        popf
+        pop BX
+        pushf
+        mul BX
+        mov CX,DX
+        popf
+        adc [DI+3],AL 
+        adc [DI+2],AH 
+        pushf
         
         
+        mov AX,[SI]
+        and AX,0Fh  
         
+        popf
+        pop BX
+        pushf
         
-        
-        ;Multiply it by its scientific notation
-        
-        ret
+        mul BX
+        add CX,DX
+        add AX,CX
+        popf
+        adc [DI+1],AL 
+        adc [DI],AH
+        pushf 
+        inc SI
+        mov AX,offset number+9
+        cmp SI,AX
+    
+    jnz HERE2
+    popf
+    ret
 
     CONV_ASC2BIN endp
-    
-    ;Transfer the Number to its proper place already converted;
-    TRANSFER_TO_NUM proc near
-        
-        sub DI, BX
-        
-        
-        mov CX, BX
-        
-        transferral: 
-            mov AX, [DI]
-            mov [SI], AX 
-            inc SI
-            inc DI 
-        loop transferral    
-        
-        
-        
-        
-    TRANSFER_TO_NUM endp
-    
+  
     ; Erase all the variables ;
     Erase_Variables proc near
           
          mov CX, 11
-         lea SI, Number
+         lea DI, Number
          clearNumber:
-            mov [SI], 00h
-            inc SI   
+            mov [DI], 00h
+            inc DI   
          loop clearNumber
          
-        
+         mov DX, 0000h
+         mov AH, 00h
     
     
         ret
     Erase_Variables endp
+    
+    ADD_A_B proc near 
+        
+        mov DI,offset SUM+3
+        mov SI,offset A+3
+        mov BX,Offset B+3
+        
+        clc
+        mov CX,4
+        
+        Addition:
+         
+            mov AL,[SI]
+            adc AL,[BX]
+            mov [DI],AL 
+            
+            dec DI
+            dec SI
+            dec BX
+        
+        loop Addition
+    
+    ret
+    
+    ADD_A_B endp 
+
+    AminuB proc near
+        mov DI,offset DIFFERENCE+3
+        mov SI,offset A+3
+        mov BX,Offset B+3
+        
+        clc
+        mov CX,4
+    
+        Subtract:
+     
+            mov AL,[SI]
+            sbb AL,[BX]
+            mov [DI],AL 
+            dec DI
+            dec SI
+            dec BX
+        loop Subtract
+    
+    ret
+    
+    AminuB endp
+
+    
        
     EXIT proc near
         ; Exit the program
@@ -192,3 +282,10 @@ ends
 
     
 end main
+
+
+
+        
+
+
+
