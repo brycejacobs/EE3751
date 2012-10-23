36c39ad1ab080e97c0820f751ddeadf3bc70a2f8
Rene Miranda <rmiran8@tigers.lsu.edu>
2012-10-23 09:30:18 -0500

    Final Addition Display

--- Lab 6/EE3751_LAB6.asm
+++ Lab 6/EE3751_LAB6.asm
@@ -20,7 +20,8 @@ B           db  4  DUP(?)
 PRODUCT     dw 4 DUP(?)
 COUNT       db  4  DUP(?) 
 SUM         db  4  DUP(?)          
-NUMBER      db  11 DUP(?)
+NUMBER      db  11 DUP(?),'$'
+
 DIFFERENCE  dw  4  DUP(?)
 ends   
 
@@ -434,7 +435,12 @@ code segment
         
         loop Addition 
         
-        call bin_Dec_acii
+        call Bin_Dec_ASCII
+        
+        
+        
+        lea DX, NUMBER
+        call Display_String
         
     
     ret
@@ -532,6 +538,350 @@ code segment
     
         ret
     Erase_Variables endp
+    
+    
+    Bin_Dec_ASCII proc near
+
+
+    ;begin code:
+
+    mov si,offset SUM
+    mov di,offset NUMBER+10d
+                           
+    ;byte 8                   
+    mov bx,[si] 
+    and bx, 0F0H
+    shr bx,4 
+    ;multiply by 2
+    mov ax,02h
+    mul bl
+    push ax
+    ;multiply by 6
+    mov ax,06h
+    mul bl
+    push ax
+    ;multiply by 8
+    mov ax,08h
+    mul bl
+    push ax   
+    ;multiply by 4
+    mov ax,04h
+    mul bl
+    push ax
+    ;multiply by 3
+    mov ax,03h
+    mul bl
+    push ax
+    ;multiply by 5
+    mov ax,05h
+    mul bl
+    push ax
+    ;multiply by 4
+    mov ax,04h
+    mul bl
+    push ax
+    ;multiply by 5
+    mov ax,05h
+    mul bl
+    push ax
+    ;multiply by 6
+    mov ax,06h
+    mul bl
+    push ax
+    
+    ;byte 7 
+    mov bx,[si] 
+    and bx, 0FH
+    ;multiply by 1
+    mov ax,01h
+    mul bl
+    push ax
+    ;multiply by 6
+    mov ax,06h
+    mul bl
+    push ax   
+    ;multiply by 7
+    mov ax,07h
+    mul bl
+    push ax
+    ;multiply by 7
+    mov ax,07h
+    mul bl
+    push ax
+    ;multiply by 7
+    mov ax,07h
+    mul bl
+    push ax
+    ;multiply by 2
+    mov ax,02h
+    mul bl
+    push ax
+    ;multiply by 1
+    mov ax,01h
+    mul bl
+    push ax
+    ;multiply by 6
+    mov ax,06h
+    mul bl
+    push ax
+    
+    
+    ; byte  6
+    inc si 
+    mov bx,[si] 
+    and bx, 0F0H
+    shr bx,4 
+    ;multiply by 1
+    mov ax,01h
+    mul bl
+    push ax
+    ;multiply by 0
+    mov ax,00h
+    mul bl
+    push ax   
+    ;multiply by 4
+    mov ax,04h
+    mul bl
+    push ax
+    ;multiply by 8
+    mov ax,08h
+    mul bl
+    push ax
+    ;multiply by 5
+    mov ax,05h
+    mul bl
+    push ax
+    ;multiply by 7
+    mov ax,07h
+    mul bl
+    push ax
+    ;multiply by 6
+    mov ax,06h
+    mul bl
+    push ax  
+    
+    
+    ;byte 5 
+    mov bx,[si] 
+    and bx, 0FH
+    ;multiply by 6
+    mov ax,06h
+    mul bl
+    push ax
+    ;multiply by 5
+    mov ax,05h
+    mul bl
+    push ax   
+    ;multiply by 5
+    mov ax,05h
+    mul bl
+    push ax
+    ;multiply by 3
+    mov ax,03h
+    mul bl
+    push ax
+    ;multiply by 6
+    mov ax,06h
+    mul bl
+    push ax
+    
+    
+    ;byte 4
+    inc si 
+    mov bx,[si] 
+    and bx, 0F0H
+    shr bx,4
+    ;multiply by 4
+    mov ax,04h
+    mul bl
+    push ax
+    ;multiply by 0
+    mov ax,00h
+    mul bl
+    push ax   
+    ;multiply by 9
+    mov ax,09h
+    mul bl
+    push ax
+    ;multiply by 6
+    mov ax,06h
+    mul bl
+    push ax 
+    
+    
+    ;byte 3
+     
+    mov bx,[si] 
+    and bx, 0FH
+    
+    ;multiply by 2
+    mov ax,02h
+    mul bl
+    push ax
+    ;multiply by 5
+    mov ax,05h
+    mul bl
+    push ax   
+    ;multiply by 6
+    mov ax,06h
+    mul bl
+    push ax
+    
+    ;byte 2
+    inc si 
+    mov bx,[si] 
+    and bx, 0F0H
+    shr bx,4
+    ;multiply by 1
+    mov ax,01h
+    mul bl
+    push ax
+    ;multiply by 6
+    mov ax,06h
+    mul bl
+    push ax   
+    
+    ;byte 1
+     
+    mov bx,[si] 
+    and bx, 0FH
+    
+    ;multiply by 1
+    mov ax,01h
+    mul bl
+    push ax
+    
+    ;end load stack
+    
+    
+    
+    
+    ;back1 
+    pop ax 
+    add [di],al   
+    call ajust
+    
+    ;back2
+    pop ax
+    add [di],al   
+    call ajust
+    pop ax 
+    add [di-1],al   
+    call ajust 
+    
+    ;back3
+    pop ax
+    add [di],al   
+    call ajust
+    pop ax 
+    add [di-1],al  
+    call ajust
+    pop ax 
+    add [di-2],al  
+    call ajust
+    
+    ;back4
+    pop ax
+    add [di],al   
+    call ajust
+    pop ax 
+    add [di-1],al   
+    call ajust
+    pop ax 
+    add [di-2],al   
+    call ajust  
+    pop ax 
+    add [di-3],al   
+    call ajust
+    
+    ;back5
+    pop ax
+    add [di],al   
+    call ajust
+    pop ax 
+    add [di-1],al  
+    call ajust
+    pop ax 
+    add [di-2],al   
+    call ajust  
+    pop ax 
+    add [di-3],al   
+    call ajust
+    pop ax 
+    add [di-4],al   
+    call ajust
+    
+    
+    
+    ;back6
+    mov bx,0
+    mov cx,7
+    back6:
+    pop ax
+    add [di-bx],al   
+    call ajust
+    dec bx
+    loop back6   
+    
+    
+    
+    ;back7
+    mov bx,0
+    mov cx,8
+    back7:
+    pop ax
+    add [di-bx],al   
+    call ajust
+    dec bx
+    loop back7
+     
+    
+    ;back8
+    mov bx,0
+    mov cx,9
+    back8:
+    pop ax
+    add [di-bx],al   
+    call ajust
+    dec bx 
+    loop back8 
+    
+    ;Decimal to ASCII
+    mov bx,1 
+    mov cx,10 
+    lea di,number
+    decconver:
+    add [DI+bx],030H
+    inc bx
+    loop decconver
+     
+    ret   
+    Bin_Dec_ASCII endp 
+    
+    ;ajust method
+    ajust proc near
+    push di   
+    push cx
+    mov cx,10d
+    hereadjust:
+    
+    mov ax,[di] 
+    aam
+    mov [di],al
+    add [di-1],ah
+    dec di
+    loop hereadjust
+    pop cx
+    pop di
+            
+    ret    
+    ajust endp 
+    
+    
+    
+    
+    
+    
     
        
     EXIT proc near
