f81a6db5591bb238dc5a749714fa8b3d8da7c146
Rene Miranda <rmiran8@tigers.lsu.edu>
2012-10-31 22:55:28 -0500

    MAIN FUNCTION WORKING NEED TO FINISH USER INTERFACE

--- LAB8/LAB8.asm
+++ LAB8/LAB8.asm
@@ -9,132 +9,63 @@ org 100h
 
 jmp start
 
+    optionmenu db 9,9,9,"MENU"
+    option1 db 10d,13d,10d,13d,"1 - Select count to be upwards (0) or downwards (1)" 
+    option2 db 10d,13d,"2 - Select start to be at 0 (0) or at Keyboard input (1)"
+    option3 db 10d,13d,"3 - Select end to be at 255 (0) or at keyboard input (1)"
+    option4 db 10d,13d,"4 - Count"
+    optionquit db 10d,13d,10d,13d, "Q - QUIT"
+    optionchoice db 10d,13d,10d,13d,9,9,"    ENTER CHOICE:$"  
+    
+    result1u db 10d,13d,10d,13d,"Counting UP$"
+    result1d db 10d,13d,10d,13d,"Counting Down$" 
+    starting db 10d,13d,10d,13d,"Starting at$"
+    ending   db 10d,13d,10d,13d,"Ending at$"
+    DBDIRECTION DB 00H
+    DBSTARTING  DB 00H
+    DBENDING    DB 0FFH
 
-    option1 db 10d,13d,"1 - Select count to be upwards (0) or downwards (1)$" 
-    option2 db 10d,13d,"2 - Select start to be at 0 (0) or at Keyboard input (1)$"
-    option3 db 10d.13d,"3 - Select end to be at 255 (0) or at keyboard input (1)$"
-    option4 db 10d.13d,"4 - Count$"
-    optionq db 10d.13d,10d.13d, "Q - QUIT$"
-    optionc db 10d.13d,"ENTER CHOICE:$"
-    result1u db 10d.13d,10d.13d,"Counting UP"
-    result1d
 start:
 
+mov AH, 00h
+mov AL, 03h
+int 10h  
 
-lea bx, target_dec
-lea di, target
-lea dx, pKey
-mov ah, 9
-int 21h 
-restart:
-;clean registers and off burning
-mov dx,127d
-mov al,0
-out dx,al
-
-mov [di],030h
-mov [di+1],030h
-mov [di+2],030h
-mov [di+3],030h
-mov [bx],030H
-
-
-
-;get user input
-MOV CX,3 
-mov AL, 00h
-INPUT:
-MOV AH, 01H
-INT 21H 
-
-cmp al,'-'
-jnz input_sign
-mov [di],'-'
-jmp INPUT
-
-;move the input to the target reg
-input_sign:
-
-cmp al,0dH
-jz afterinput
-
-mov ah,[di+2]
-mov [di+1],ah
-
-mov ah,[di+3]
-mov [di+2],ah
-
-
-mov [di+3],al
-
-loop INPUT
-
-afterinput:
-
-;**************
-;converting to decimal to hex
-mov al,[di+3]
-sub al,30h
-mov [bx],ax
-
-mov al,[di+2] 
-sub al,30h
-mov cx,0AH
-mul cl
-add [bx],al 
-
-mov al,[di+1]
-sub al,30h 
-mov cx,064H
-mul cl 
-add [bx],al
-;converting to 2nd complement
-cmp [di],'-'
-jnz comparing
-not [bx]
-add [bx],1 
-
-
-
-;comparing temperature
-comparing:
-lea dx, pKey
-mov ah, 9
+lea DX, optionmenu
+mov AH, 09h
 int 21h
 
-here:
-mov AH, 01h
-int 16h
-jnz restart
-
-mov dx,125d
-in al,dx
-  
-  
-cmp [bx],al  
-jle donothing
-
-mov dx,127d
-mov al,1
+;counting routine
+;###################################
+mov dx,199d
+mov ah,00h
+mov al,[DBSTARTING]
 out dx,al
-jmp here
 
-donothing:
-mov dx,127d
-mov al,0
+cmp [DBDIRECTION],1
+jz downward
+upward:
+inc al
 out dx,al
-jmp here
+cmp [DBENDING],al
+jz stop
+cmp al,0FFh
+jz stop
+jmp upward
+
+downward:
+dec al
+out dx,al
+cmp [DBENDING],al
+jz stop
+cmp al,0h
+jz stop
+jmp downward:
+
+stop:
+RET
 
 
-ret  ; return to the operating system.
 
-
-
-print proc near
-
-
-
-ret  ; return to the main program.
-print endp
 
 
